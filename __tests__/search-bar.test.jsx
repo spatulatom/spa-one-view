18d6bbfb526ef2ee5a5ae9283765fd34334515f3
spatulatom <spatulatom@gmail.com>
2023-02-15 12:48:56 +0000

    formatting

--- __tests__/search-bar.test.jsx
+++ __tests__/search-bar.test.jsx
@@ -1,18 +1,18 @@
 import SearchBar from '../components/search-bar';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import mockRouter from 'next-router-mock';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 // mock router: https://github.com/scottrippey/next-router-mock
 jest.mock('next/router', () => require('next-router-mock'));
 
 describe('Search Bar', () => {
-  //   it('find incremnt/decrement numbers button in the input field', () => {
-  //     render(<SearchBar />);
+  it('find incremnt/decrement numbers button in the input field', () => {
+    render(<SearchBar />);
 
-  //     const button = screen.queryByRole('spinbutton');
-  //     expect(button).not.toBeNull();
-  //   });
+    const button = screen.queryByRole('spinbutton');
+    expect(button).not.toBeNull();
+  });
 
   it('see if the input changes when value entered', () => {
     render(<SearchBar />);
@@ -23,8 +23,8 @@ describe('Search Bar', () => {
       target: { value: '9' },
     });
 
-   const text = screen.queryByText(/9/);
-   expect(text).toBeInTheDocument()
+    const text = screen.queryByText(/9/);
+    expect(text).toBeInTheDocument();
 
     // screen.debug();
   });
